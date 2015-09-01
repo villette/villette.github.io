4ce7c6a9a8e4d7989dc3cfc7c1fad70c7aa3e7d2
Pierre Villette <villette@pheromone.ca>
2015-09-01 14:27:53 -0400

    Change portfolio pagination for something more beautiful

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,27 +9,14 @@ $(document).ready(function() {
     $(this).children('a').children('span').fadeOut(200);
   });
 
-  // Portfolio pagination
-  $(window).bind('hashchange', function(e) {
-    var url = $.param.fragment() || 'page-1';
-    var first = ('#' + url == $('ul.pagination li.first a').attr('href'));
-    var last = ('#' + url == $('ul.pagination li.last a').attr('href'));
+  // Portfolio dynamic display
+  $('div.portfolio div.show-more button').click(function(e) {
+    $('div.portfolio div.project-group:hidden:first').slideDown();
+    $('html, body').animate({ scrollTop: $(document).height() });
 
-    $('ul.pagination li.previous').toggleClass('disabled', first);
-    $('ul.pagination li.next').toggleClass('disabled', last);
-
-    var previousHref = first ? '#' + url : '#' + url.replace(/\d+/, function(a) { return parseInt(a) - 1; });
-    $('ul.pagination li.previous a').attr('href', previousHref);
-    var nextHref = last ? '#' + url : '#' + url.replace(/\d+/, function(a) { return parseInt(a) + 1; });
-    $('ul.pagination li.next a').attr('href', nextHref);
-
-    $('ul.pagination li').removeClass('active');
-    $('ul.pagination li').has('a[href="#' + url + '"]').addClass('active');
-
-    $('div.portfolio').children(':visible').hide();
-    $('div.portfolio div.' + url).show();
+    if ($('div.portfolio div.project-group:hidden').length == 0) {
+      $('div.portfolio div.show-more').slideUp();
+    }
   });
-
-  $(window).trigger('hashchange');
 
 });
