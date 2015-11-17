e61b69759e7f7ada5acc87d48c34a600de86b0f0
Pierre Villette <villette@pheromone.ca>
2015-11-17 13:36:43 -0500

    Translate contact form messages

--- js/contact.js
+++ js/contact.js
@@ -1,29 +1,17 @@
 $(document).ready(function() {
 
-  $(window).trigger('hashchange');
-
   // Handle form with AJAX
   $('form.contact').submit(function(e) {
     e.preventDefault();
 
     $.ajax({
-      url: '//formspree.io/villette.pierre@gmail.com',
+      url: $(this).attr('action'),
       method: 'POST',
       data: $(this).serialize(),
       dataType: 'json',
       complete: function(xhr, status) {
-        switch (status) {
-          case 'success':
-            var attribute = 'success';
-            var message = 'Votre message a bien été envoyé.';
-            break;
-
-          case 'error':
-          default:
-            var attribute = 'danger';
-            var message = 'Une erreur est apparue, veuillez réessayer plus tard.';
-            break;
-        }
+        var message = $('div#form').data(status);
+        var attribute = (status == 'error') ? 'danger' : status;
 
         $('div.alert').remove();
         $('form.contact').before('<div id="' + status + '" class="alert alert-' + attribute + '" style="display: none;"><a href="#" data-dismiss="alert" class="close">×</a>' + message + '</div>');
