10073ebfb41a33cafeaa8034d7ecb1b16d1f8adf
Pierre Villette <pierre.villette@thefitchgroup.com>
2023-12-29 10:45:02 +0000

    Add docker-compose file and misc improvements.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -17,12 +17,12 @@ export default function Navigation({ links, currentLanguage, setCurrentLanguage,
 
   return (
     <nav ref={sideNav} id='sideNav' className='navbar navbar-expand-lg navbar-dark bg-primary fixed-top'>
-      <a className='navbar-brand js-scroll-trigger' href='#page-top'>
+      <span className='navbar-brand js-scroll-trigger'>
         <span className='d-block d-lg-none'>Pierre Villette</span>
         <span className='d-none d-lg-block'>
           <img className='img-fluid img-profile rounded-circle mx-auto mb-2' src='/img/profile.jpg' alt='Pierre Villette' height='144' width='144' />
         </span>
-      </a>
+      </span>
 
       <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
         <span className='navbar-toggler-icon'></span>
