03adc9f64380963a2f4e193cc91b37fa21ec7010
Pierre Villette <pierre.villette@thefitchgroup.com>
2024-01-04 02:43:53 +0000

    Get rid of unnecessary HTML ids.

--- src/_js/modules/scrollspy.mjs
+++ src/_js/modules/scrollspy.mjs
@@ -2,7 +2,7 @@ import { ScrollSpy } from 'bootstrap';
 
 export default () => {
   new ScrollSpy(document.body, {
-    target: '#sideNav',
+    target: '#navbar',
     rootMargin: '0px 0px -40%',
   });
 };
